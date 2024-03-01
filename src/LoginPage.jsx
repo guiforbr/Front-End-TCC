d4b63056ed9e7b9134fba4f8d3f13bcb7c729fe5
guiforbr <guiforbr@gmail.com>
2024-03-01 20:10:14 -0300

    segundo commit

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,5 +1,5 @@
 import './LoginPage.css'
-import fotoBag from './assets/school-bag.png'
+import fotoBag from './assets/school-bag.png' 
 import fotoPc from './assets/online-study.png'
 import fotoFolha from './assets/blood-report.png'
 
@@ -7,7 +7,7 @@ function LoginPage() {
 
   return (
     <div>
-      <div className='inputLogin'>
+      <div className='inputLogin'> {/* Div dos dados do login */}
         <form action="">
           <h3 className='email'>E-mail</h3>
             <input type='email' id='email' pattern='.+@exemplo\.com' size="30" placeholder='Insira seu e-mail' className='inputEmail' required/>
@@ -15,13 +15,13 @@ function LoginPage() {
             <input type="password" name="" id="" className='inputSenha' placeholder='Insira sua senha' required/>
 
           </form>
-      </div>
-        <div className='container'>
+      </div> {/* Fim da div dos dados de login*/}
+        <div className='container'> {/* Div das imagens background */}
           <img src={fotoBag} alt='Imagem de uma mochila' className='bag'/>
           <img src={fotoPc} alt="Imagem de um notebook" className='note' />
           <button type='submit' className='buttonLogin'>Login</button>
           <img src={fotoFolha} alt="Imagem de uma folha" className='folha'/>
-        </div> 
+        </div> {/* fim da div das imagens background */}
     </div>
   )
 }
